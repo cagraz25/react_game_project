44c258aa221140c799a6612b0aff05fa5beef718
Phone Tshan Kyaw <phonetshankyawpism@gmail.com>
2025-07-07 21:21:09 -0700

    New style

--- src/bruin.js
+++ src/bruin.js
@@ -1,34 +1,51 @@
-/*
-window.onload = function() {
-    setGame();
-}
+const cursor = document.querySelector('.cursor')
+const den = [...document.querySelectorAll('.den')]
+const scoreEl = document.querySelector('.score span')
+let score = 0
 
-*/
+const sound = new Audio("./smash.mp3")
 
-import React from 'react';
-import { useState, useEffect } from 'react';
+function run() {
+    const i = Math.floor(Math.random() * den.length);
+    const targ_den = den[i];
+    let timer = null;
 
-export default function Game() {
-    const [tiles, setTiles] = useState([]);
+    const img = document.createElement('img')
+    img.classList.add('joe')
+    img.src = "/joe.png"
 
-    useEffect(() => {
-        setGame();
-    }, []);
+    img.addEventListener('click', () => {
+        score += 1;
+        sound.play();
+        scoreEl.textContent = score;
+        clearTimeout(timer);
+        setTimeout(() => {
+            if (targ_den.contains(img)) {
+            targ_den.removeChild(img);
+            }
+            run();
+        }, 500);
+    })
+    targ_den.appendChild(img)
 
-    function setGame() {
-        const newTiles = [];
-        for (let i = 0; i < 9; i++) {
-            newTiles.push({ id: i.toString() });
+    timer = setTimeout(() => {
+        if (targ_den.contains(img)) {
+            targ_den.removeChild(img);
         }
-        setTiles(newTiles);
-    }
-
-    return (
-        <div id="trees">
-            {tiles.map(tile => (
-                <div key={tile.id} id={tile.id}></div>
-            ))}
-        </div>
-    );
+        run()
+    }, 1500);
 }
+run()
 
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + 'px'
+    cursor.style.left = e.pageX + 'px'
+})
+
+window.addEventListener('mousedown', () => {
+    cursor.classList.add('active')
+})
+
+window.addEventListener('mousemove', () => {
+    cursor.classList.remove('active')
+})
